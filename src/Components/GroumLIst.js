80818684aa1c679ac44b024f642ff4e8e0fe25ee
lamia101 <lamialatif01@gmail.com>
2024-02-07 15:48:17 +0600

    image upload and download done

--- src/Components/GroumLIst.js
+++ src/Components/GroumLIst.js
@@ -1,13 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import '../Assets/Css/list.css'
-import img1 from '../Assets/Images/Abrar.JPG';
-import img2 from '../Assets/Images/Adnan.JPG';
-import img3 from '../Assets/Images/Jakey.JPG';
-import img4 from '../Assets/Images/Jubaer.JPG';
-import img5 from '../Assets/Images/Hasan.JPG';
-import img6 from '../Assets/Images/Tamjid.JPG';
-import img7 from '../Assets/Images/Taufiq.JPG';
-import img8 from '../Assets/Images/Rubayed.JPG';
 import {Link} from "react-router-dom";
 import axios from "axios";
 
